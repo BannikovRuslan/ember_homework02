259737d58669e153f0c9be2e7a2be7a32bcbd91c
Ruslan Bannikov <bannikov.ru@inbox.ru>
2022-10-07 18:45:02 +0500

    root repair

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'ember-homework02',
     environment,
-    rootURL: '/ember-homework02',
+    rootURL: '/ember_homework02',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
